8586948333e31b3a22c2b8aa7473380fe51258b1
lokeshrathinam017 <lokesrathinam017@gmail.com>
2026-09-10 18:13:45 +0530

    Initial project commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import Header from "./components/Header";
+import Card from "./components/Card";
+import TodoContainer from "./components/TodoContainer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Landing from "./pages/Landing";
+import { useState } from "react";
+
+const App=()=>
+{
+
+  const [users,setusers] = useState(
+    [
+        {
+            username:"Loki",
+            password:"123"
+        },
+    ]
+)
+
+  return(
+    <div>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Login users={users} setusers={setusers}/>}></Route>
+    <Route path='/signup' element={<Signup users={users} setusers={setusers}/>}></Route>
+    <Route path='/landing' element={<Landing/>}></Route>
+    </Routes>
+    </BrowserRouter>
+  </div>
+  )
+}
+
+export default App;
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,28 @@
+const TodoItem = (props)=>
+{
+    const activityArr = props.activityArr
+    const setActivityArr = props.setActivityArr
+
+    const handleDelete=(deleteid)=>
+    {
+        let temparr = activityArr.filter(function(item){
+            if(item.id === deleteid)
+            {
+                return false 
+            }else{
+                return true 
+            }
+        })
+
+        setActivityArr(temparr)
+    }
+
+    return(
+        <div className="flex justify-between">
+        <p>{props.index+1}.{props.activity}</p>
+        <button className="text-red-500" onClick={()=>handleDelete(props.id)} >Delete</button>
+        </div>
+    )
+}
+
+export default TodoItem
